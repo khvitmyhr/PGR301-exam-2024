256aae43dacf56799962a636fd5d277baadc21e9
EC2 Default User <ec2-user@ip-172-31-44-205.eu-west-1.compute.internal>
2024-11-14 15:03:38 +0000

    terraformworkflow

--- infra/main.tf
+++ infra/main.tf
@@ -1,7 +1,3 @@
-data "aws_region" "current" {}
-
-data "aws_caller_identity" "current" {}
-
 resource "aws_iam_role" "lambda_exec_role" {
   name = "lambda_sqs_exec_role_13"
   assume_role_policy = jsonencode({
@@ -39,6 +35,7 @@ resource "aws_iam_policy" "lambda_sqs_policy" {
         Resource = "arn:aws:s3:::pgr301-couch-explorers/13/generated_images/*"
       },
       {
+        # Legger til CloudWatch logging tillatelser
         Action = [
           "logs:CreateLogGroup",
           "logs:CreateLogStream",
@@ -48,6 +45,7 @@ resource "aws_iam_policy" "lambda_sqs_policy" {
         Resource = "*"
       },
       {
+        # Tillatelser for Bedrock
         Action = [
           "bedrock:InvokeService",
           "bedrock:InvokeModel"
@@ -64,12 +62,12 @@ resource "aws_iam_role_policy_attachment" "attach_policy" {
   policy_arn = aws_iam_policy.lambda_sqs_policy.arn
 }
 
-# Opprette SQS-kø
+#Opprette kø
 resource "aws_sqs_queue" "my_sqs_queue" {
   name = "image_processing_queue_13"
 }
 
-# Opprett en zip-fil fra Python-koden
+# Create a zip file from the Python code (using the local lambda_function.py)
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_file = "${path.module}/../lambda_sqs.py"
@@ -79,35 +77,37 @@ data "archive_file" "lambda_zip" {
 resource "aws_lambda_function" "image_processing_lambda" {
   function_name    = "image_processing_lambda_13"
   role             = aws_iam_role.lambda_exec_role.arn
-  handler          = "lambda_sqs.lambda_handler"
+  handler          = "lambda_sqs.lambda_handler"  # Erstatt med riktig handler-funksjon fra Python-koden
   runtime          = "python3.8"
   filename         = data.archive_file.lambda_zip.output_path
   timeout          = 60
   
-  environment {
+   environment {
     variables = {
       BUCKET_NAME = var.bucket_name  # Bruker bucket_name-variabelen fra variables.tf
     }
   }
 }
 
+
+
 resource "aws_lambda_event_source_mapping" "sqs_event_mapping" {
   event_source_arn = aws_sqs_queue.my_sqs_queue.arn
   function_name    = aws_lambda_function.image_processing_lambda.arn
-  batch_size       = 10
+  batch_size       = 10  # Tilpass batch-størrelsen etter behov
 }
 
+
 resource "aws_lambda_function_url" "comprehend_lambda_url" {
-  function_name     = aws_lambda_function.image_processing_lambda.function_name
-  authorization_type = "NONE"
+  function_name = aws_lambda_function.image_processing_lambda.function_name
+  authorization_type = "NONE"  # Tilpass om du trenger autentisering
 }
 
 output "lambda_url" {
   value = aws_lambda_function_url.comprehend_lambda_url.function_url
 }
 
-# Konstruer riktig URL for SQS-køen ved å bruke ARN
 output "sqs_queue_url" {
-  value = "https://sqs.${data.aws_region.current.name}.amazonaws.com/${data.aws_caller_identity.current.account_id}/${aws_sqs_queue.my_sqs_queue.name}"
+  value       =  aws_sqs_queue.my_sqs_queue.url
   description = "The URL of the SQS queue"
 }
