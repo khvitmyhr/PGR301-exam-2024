8e79d34006f713aaffdee406113697b6b92e2600
EC2 Default User <ec2-user@ip-172-31-44-205.eu-west-1.compute.internal>
2024-11-14 14:48:12 +0000

    terraformworkflow

--- infra/main.tf
+++ infra/main.tf
@@ -106,3 +106,8 @@ resource "aws_lambda_function_url" "comprehend_lambda_url" {
 output "lambda_url" {
   value = aws_lambda_function_url.comprehend_lambda_url.function_url
 }
+
+output "sqs_queue_url" {
+  value       = aws_sqs_queue.my_sqs_queue.url
+  description = "The URL of the SQS queue"
+}
