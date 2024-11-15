13b963eb60954443942c246da4915a7d6aca6696
EC2 Default User <ec2-user@ip-172-31-44-205.eu-west-1.compute.internal>
2024-11-15 15:10:26 +0000

    added alarm code i main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -35,7 +35,7 @@ resource "aws_iam_policy" "lambda_sqs_policy" {
         Resource = "arn:aws:s3:::pgr301-couch-explorers/13/generated_images/*"
       },
       {
-        # Legger til CloudWatch logging tillatelser
+
         Action = [
           "logs:CreateLogGroup",
           "logs:CreateLogStream",
@@ -45,7 +45,7 @@ resource "aws_iam_policy" "lambda_sqs_policy" {
         Resource = "*"
       },
       {
-        # Tillatelser for Bedrock
+
         Action = [
           "bedrock:InvokeService",
           "bedrock:InvokeModel"
@@ -62,12 +62,13 @@ resource "aws_iam_role_policy_attachment" "attach_policy" {
   policy_arn = aws_iam_policy.lambda_sqs_policy.arn
 }
 
-#Opprette kø
+
 resource "aws_sqs_queue" "my_sqs_queue" {
   name = "image_processing_queue_13"
+  visibility_timeout_seconds = 60
 }
 
-# Create a zip file from the Python code (using the local lambda_function.py)
+
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_file = "${path.module}/../lambda_sqs.py"
@@ -77,30 +78,74 @@ data "archive_file" "lambda_zip" {
 resource "aws_lambda_function" "image_processing_lambda" {
   function_name    = "image_processing_lambda_13"
   role             = aws_iam_role.lambda_exec_role.arn
-  handler          = "lambda_sqs.lambda_handler"  # Erstatt med riktig handler-funksjon fra Python-koden
+  handler          = "lambda_sqs.lambda_handler"  
   runtime          = "python3.8"
   filename         = data.archive_file.lambda_zip.output_path
   timeout          = 60
   
    environment {
     variables = {
-      BUCKET_NAME = var.bucket_name  # Bruker bucket_name-variabelen fra variables.tf
+      BUCKET_NAME = var.bucket_name 
     }
   }
 }
 
+#Oppgave 4
+#------------------------------------------------------------------
 
 
+resource "aws_sns_topic" "sqs_alarm_topic" {
+  name = "sqs_alarm_topic_13"
+}
+
+resource "aws_sns_topic_subscription" "email_subscription" {
+  endpoint = "kihv001@student.kristiania.no"  
+  protocol = "email"
+  topic_arn = aws_sns_topic.sns_topic.arn
+}
+
+
+resource "aws_cloudwatch_metric_alarm" "sqs_oldest_message_age_alarm" {
+  alarm_name          = "SQS-Oldest-Message-Age-Alarm_13"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "ApproximateAgeOfOldestMessage"
+  namespace           = "AWS/SQS"
+  period              = "30"  # Måling hvert minutt
+  statistic           = "Maximum"
+  threshold           = 10  # Alarmen utløses når eldste melding er eldre enn 5 sekunder (300 sek)
+  alarm_description   = "Triggeres når eldste melding i SQS-køen er eldre enn 5 sekunder"
+  dimensions = {
+    QueueName = aws_sqs_queue.my_sqs_queue.name  
+  }
+
+
+  alarm_actions = [aws_sns_topic.sqs_alarm_topic.arn]  
+  ok_actions     = [aws_sns_topic.sqs_alarm_topic.arn]  
+}
+
+
+resource "aws_sns_topic" "sns_topic" {
+  name = "sqs_alarm_topic"
+}
+
+
+
+#------------------------------------------------------------------
+
+#For oppgave 4, ved å fjerne denne prosesserer ikke meldingen i lamda og meldingen blir liggende i kø, slik får vi utløst alarm!
 resource "aws_lambda_event_source_mapping" "sqs_event_mapping" {
   event_source_arn = aws_sqs_queue.my_sqs_queue.arn
   function_name    = aws_lambda_function.image_processing_lambda.arn
-  batch_size       = 10  # Tilpass batch-størrelsen etter behov
+  batch_size       = 10 
+  
 }
+
 
 
 resource "aws_lambda_function_url" "comprehend_lambda_url" {
   function_name = aws_lambda_function.image_processing_lambda.function_name
-  authorization_type = "NONE"  # Tilpass om du trenger autentisering
+  authorization_type = "NONE"  
 }
 
 output "lambda_url" {
--- infra/provider.tf
+++ infra/provider.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.74.0"  # Bruker AWS-provider versjon 4.x
+      version = "5.74.0" 
     }
   }
 
